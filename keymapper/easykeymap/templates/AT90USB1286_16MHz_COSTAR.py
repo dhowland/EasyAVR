91a4b2fc0afef03b4b6f79c0f700e3b1cda993a7
David Howland <dhowland@gmail.com>
2016-04-10 16:12:03 -0400

    Add new build of the COSTAR size for the AT90USB1286

---
+++ keymapper/easykeymap/templates/AT90USB1286_16MHz_COSTAR.py
@@ -0,0 +1,29 @@
+# This file is auto-generated.  Do not edit.
+
+hex_file_name = 'AT90USB1286_16MHz_COSTAR.hex'
+device = 'AT90USB1286'
+speed = '16MHz'
+size = 'COSTAR'
+
+layers_map = 0x00000d20
+actions_map = 0x00000780
+tapkeys_map = 0x000001e0
+macro_map = 0x000012c0
+num_leds_map = 0x000033dc
+num_ind_map = 0x000033db
+led_hw_map = 0x000033b7
+led_map = 0x0000338d
+num_bl_enab_map = 0x0000338c
+bl_mask_map = 0x00003380
+bl_mode_map = 0x000032c0
+strobe_cols_map = 0x0000347c
+strobe_low_map = 0x0000347b
+num_strobe_map = 0x0000347a
+num_sense_map = 0x00003479
+matrix_init_map = 0x0000346d
+matrix_strobe_map = 0x00003401
+matrix_sense_map = 0x000033dd
+kmac_key_map = None
+pw_defs_map = 0x0000347d
+boot_ptr_map = 0x00003c3c
+prod_str_map = 0x00003dbd
