6f0f3a00fe99cb6fa3453bb5bf3cb491b7d42f4c
David Howland <dhowland@gmail.com>
2018-01-04 11:17:20 -0500

    Bump mini version number for bug fix

--- keymapper/easykeymap/__init__.py
+++ keymapper/easykeymap/__init__.py
@@ -15,6 +15,6 @@
 # with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 # Version is functionally required to be formatted as #.##.##
-__version__ = '2.05.09'
+__version__ = '2.05.10'
 
 __all__ = ['gui']
