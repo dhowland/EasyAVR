a82a33b6e0665608a3cc6b2c0f0724a37a5565c3
David Howland <dhowland@gmail.com>
2021-08-28 12:49:54 -0400

    Rebuild docs

--- docs/build/html/_static/documentation_options.js
+++ docs/build/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '3.03.02',
+    VERSION: '3.04.01',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
